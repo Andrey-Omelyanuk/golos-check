8c03206b24a2840b7e1e0a4d8670b7f4961e7cf5
Andrey Omelyanuk <omelyanukandrey@gmail.com>
2021-12-06 00:30:49 +0300

    in progress

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # golos-check
+
+# 1. download data from the golos
+python download-data.py
+# 2. push data to DB
+python push-data-to-db.py
+# 3. up notebook
+docker-compouse up
--- download-data.py
+++ download-data.py
@@ -1,51 +1,67 @@
 import requests, json, time, os
 
 
-files = {
-	'pollingStations.json'	: 'https://belarus2020.org/pollingStations.json',
-	'whitelist.json'		: 'https://belarus2020.org/data/results/whitelist.json',
-	'total.json'			: 'https://belarus2020.org/data/results/total.json',
-	'unassigned.json'		: 'https://belarus2020.org/data/results/byStation/unassigned.json',
-	'stats.json'			: 'https://d3kxokrdpc64vo.cloudfront.net/stats.json',
-}
-# for file_name, url in files.items():
-# 	response = requests.get(url)
-# 	with open(f'data/{file_name}', 'w', encoding='utf-8') as f:
-# 		json.dump(response.json(), f, ensure_ascii=False, indent=4)
+# get data from file of download from url and save it to file
+def get_data(file_name, url):
+    data = None
+    if not os.path.exists(file_name):
+        print(f"Download {url}")
+        response = requests.get(url)
+        with open(file_name, 'w', encoding='utf-8') as f:
+            try:
+                data = response.json()
+                json.dump(data, f, ensure_ascii=False, indent=4)
+                print(f"Saved in {file_name}")
+            except Exception as e:
+                print(f"Empty data")
+                data = {} 
+    else:
+        try:
+            with open(file_name, 'r', encoding='utf-8') as f:
+                data = json.load(f)
+        except Exception as e:
+            print(file_name, "Empty data")
+            data = {} 
+    return data
 
+
+# download base data
+pollingStations = get_data('data/pollingStations.json'  , 'https://belarus2020.org/pollingStations.json')
+whitelist       = get_data('data/whitelist.json'        , 'https://belarus2020.org/data/results/whitelist.json')
+total           = get_data('data/total.json'            , 'https://belarus2020.org/data/results/total.json')
+unassigned      = get_data('data/unassigned.json'       , 'https://belarus2020.org/data/results/byStation/unassigned.json')
+stats           = get_data('data/stats.json'            , 'https://d3kxokrdpc64vo.cloudfront.net/stats.json')
 
 # gather station codes
 stations_code = set()
-with open(f'data/pollingStations.json', 'r', encoding='utf-8') as f:
-	stations = json.load(f)
-with open(f'data/whitelist.json', 'r', encoding='utf-8') as f:
-	whitelist= json.load(f)
-
-for station in stations:
-	stations_code.add(station['code'])
+for station in pollingStations: 
+    stations_code.add(station['code'])
 stations_code.update(whitelist['forgery'])
 stations_code.update(whitelist['truth'])
 # fix a bug in the data
 stations_code.remove('проспект Мира, дома 15, 21, 25, 25а, 256, 25в, 25г, 27, 27а, 276, 29; улица Левая Дубравенка от проспекта Мира до Клубного Оврага; 4-й Октябрьский переулок; улица Бурденко, дома 10, 12.')
 
 
-target = {
-	'byStation'	: 'https://belarus2020.org/data/results/byStation/{}.json',
-	'photos'   	: 'https://belarus2020.org/data/results/photos/{}.json',
-	'Quarantine': 'https://api.golos2020.org/Quarantine/{}',
-}
+# download details of stations
+stations = {} 
 for code in stations_code:
-	# time.sleep(5)  # avoid throttling
-	for group, url_pattern in target.items():
-		if (os.path.exists(f'data/{group}/{code}.json')):
-			continue
-		url = url_pattern.format(code)
-		print(url)
-		response = requests.get(url)
-		try:
-			with open(f'data/{group}/{code}.json', 'w', encoding='utf-8') as f:
-				json.dump(response.json(), f, ensure_ascii=False, indent=4)
-		except:
-			print('Error')
+    by_station = get_data(f'data/byStation/{code}.json', f'https://belarus2020.org/data/results/byStation/{code}.json')
+    x = {
+        'photos'    : get_data(f'data/photos/{code}.json'       , f'https://belarus2020.org/data/results/photos/{code}.json'),
+        'quarantine': get_data(f'data/Quarantine/{code}.json'   , f'https://api.golos2020.org/Quarantine/{code}'),
+    }
+    stations[code] = {**by_station, **x}
 
-# download photos
+
+# TODO: download photos
+
+
+# gather everything in one file /work/data.json 
+with open('work/data.json', 'w', encoding='utf-8') as f:
+    json.dump({
+        'stations'  : stations,
+        'whitelist' : whitelist,
+        'total'     : total,
+        'unassigned': unassigned,
+        'stats'     : stats
+    }, f, ensure_ascii=False, indent=4)
